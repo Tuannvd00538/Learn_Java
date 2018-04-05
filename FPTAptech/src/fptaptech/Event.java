79a7c45735516bf1ddb844bb5201fda508afd6fe
Ngo Van Tuan <tuannvd00538@fpt.edu.vn>
2018-04-05 10:50:14 +0700

    Event FPT Aptech

---
+++ FPTAptech/src/fptaptech/Event.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fptaptech;
+
+/**
+ *
+ * @author Ngo Van Tuan
+ */
+public class Event {
+    private int id;
+    private String name;
+    private String createAt;
+    private String endAt;
+    private String category;
+    private int status;
+
+    public Event() {
+    }
+
+    public Event(String name, String createAt, String endAt, String category) {
+        this.name = name;
+        this.createAt = createAt;
+        this.endAt = endAt;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getEndAt() {
+        return endAt;
+    }
+
+    public void setEndAt(String endAt) {
+        this.endAt = endAt;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
